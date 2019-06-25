8b724d1ccdfe0892d95ad85b3ff0635ec62eb607
yurachern <tchernykh.yurij@ukr.net>
2019-06-25 03:53:21 +0300

    initial project version

---
+++ WpfLabaMVVM/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace WpfLabaMVVM
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+    }
+}
